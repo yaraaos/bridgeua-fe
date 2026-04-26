6c628d14fd2af5b843888d4b63a79ef6925c7516
yaraaos <yara144ss@gmail.com>
2026-04-26 23:56:17 +0200

    feat: add animated splash screen with auto navigation

--- app/splash/splash.styles.ts
+++ app/splash/splash.styles.ts
@@ -1,45 +1,23 @@
-import { StyleSheet } from "react-native";
-import { colors } from "../../src/constants/colors";
+import { Dimensions, StyleSheet } from "react-native";
+
+export const SVG_WIDTH = 280;
+export const SCREEN_WIDTH = Dimensions.get("screen").width;
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-
-  image: {
-    flex: 1,
-  },
-
-  overlay: {
-    flex: 1,
-    justifyContent: "space-between",
-    paddingTop: 120,
-    paddingBottom: 40,
-    paddingHorizontal: 24,
-    backgroundColor: "rgba(0,0,0,0.25)",
-  },
-
-  center: {
     alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
   },
-
-  logo: {
-    fontSize: 40,
-    fontWeight: "800",
-    color: "#FFFFFF",
+  logoWrap: {
+    width: SVG_WIDTH,
+    height: 210,
+    alignItems: "center", // 👈 important
+    justifyContent: "center",
   },
-
-  ua: {
-    color: colors.primaryGreen,
-  },
-
-  subtitle: {
-    marginTop: 8,
-    color: "#FFFFFF",
-    opacity: 0.9,
-  },
-
-  footer: {
-    width: "100%",
+  textWrap: {
+    alignItems: "center",
+    marginTop: 6,
   },
 });
