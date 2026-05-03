a2ebf38ed1389c8ec456fab0d535fc5f6094135b
yaraaos <yara144ss@gmail.com>
2026-05-04 00:41:06 +0200

    chore: export UI primitives from shared index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as AppCheckBox } from "./AppCheckBox/AppCheckBox";
+export { default as AppRadio } from "./AppRadio/AppRadio";
+export { default as AppSelect } from "./AppSelect/AppSelect";
+export { default as AppStatCard } from "./AppStatCard/AppStatCard";
+export { default as AppTimeSlot } from "./AppTimeSlot/AppTimeSlot";
+export { default as AppDatePickerCard } from "./AppDatePickerCard/AppDatePickerCard";
